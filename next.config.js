1170dee1f0e472331d7df7ac44f821d56f7a2884
DEFAULTE-R <harilogesh2019@gmail.com>
2026-08-29 11:40:55 +0000

    Fix GitHub Pages asset paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/vortexone',
+  assetPrefix: '/vortexone/',
   images: {
     unoptimized: true,
   },
